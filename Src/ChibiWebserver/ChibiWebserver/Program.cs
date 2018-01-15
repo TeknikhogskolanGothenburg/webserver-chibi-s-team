a8345024774a964b20028bf475de95854f5ea7a3
Rasmus Berg <chibi86@hotmail.com>
2018-01-15 13:16:59 +0100

    Session cookies and Counter-page added
    Add: Session cookies
    Add: Counter-page

--- Src/ChibiWebserver/ChibiWebserver/Program.cs
+++ Src/ChibiWebserver/ChibiWebserver/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ChibiWebserver
 {
@@ -10,12 +9,17 @@ namespace ChibiWebserver
     {
         static void Main(string[] args)
         {
-            string[] prefixes = new string[2] { "http://127.0.0.1:8080/", "http://localhost:8080/" };
+            string[] prefixes = new string[] { "http://127.0.0.1:8080/", "http://localhost:8080/" };
 
+            // Make webserver and start it
             WebServer webserver = new WebServer(prefixes);
-            webserver.Run();
+            webserver.Start();
+
+            // Ask when to stop, and wait for it
             Console.WriteLine("Simply press a key to shutdown webserver.");
             Console.ReadKey();
+
+            // Stop webserver
             webserver.Stop();
             Console.ReadKey();
         }

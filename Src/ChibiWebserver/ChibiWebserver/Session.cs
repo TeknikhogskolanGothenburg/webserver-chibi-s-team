8bdb0379d559c4f16ea6c214335ed97d05c2229b
Rasmus Berg <chibi86@hotmail.com>
2018-01-15 20:48:07 +0100

    Add dynamic and server info pages and a Session class
    Add: Dynamic page 'Dynamic/'
    Add: Server info page 'Info/'
    Add: Session class (rebuild and move from WebServer class)
    Add: Special page response and file response move to seperated methods

---
+++ Src/ChibiWebserver/ChibiWebserver/Session.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ChibiWebserver
+{
+    class Session
+    {
+        private Dictionary<string, string> session;
+        private int lastSessionIndex;
+        private string thisRunsUniqueKey;
+        private HttpListenerRequest request;
+        private HttpListenerResponse response;
+
+        /// <summary>
+        /// Properties for session.count - get
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return session.Count;
+            }
+        }
+
+        /// <summary>
+        /// Constructor with this run's unique sessions key
+        /// </summary>
+        /// <param name="key">This run's unique sessions key</param>
+        public Session(string key)
+        {
+            session = new Dictionary<string, string>();
+            thisRunsUniqueKey = key;
+            lastSessionIndex = 0;
+        }
+
+        /// <summary>
+        /// Initialize request and response from web request 
+        /// </summary>
+        /// <param name="context">Web context (HttpListenerContext)</param>
+        public void Initialize(HttpListenerContext context)
+        {
+            request = context.Request;
+            response = context.Response;
+        }
+
+        /// <summary>
+        /// Add a session value
+        /// </summary>
+        /// <param name="value">Value to input (string)</param>
+        public void Add(string value)
+        {
+            // Session key set by this run's unique key and last session index + one
+            lastSessionIndex++;
+            string sessionKey = string.Format("{0}_{1}", thisRunsUniqueKey, lastSessionIndex);
+
+            // Set session and cookie
+            session.Add(sessionKey, value);
+            response.SetCookie(new Cookie("counter", sessionKey, "/"));
+        }
+
+        /// <summary>
+        /// Check if session with cookie key exist
+        /// </summary>
+        /// <param name="cookieKey">cookie key (string)</param>
+        /// <returns>Validation result (bool)</returns>
+        public bool Exist(string cookieKey)
+        {
+            Cookie cookie = GetCookie(cookieKey);
+
+            // If cookie exist, check if session with session key exist
+            if(cookie != null)
+            {
+                string sessionKey = cookie.Value;
+
+                if (session.ContainsKey(sessionKey))
+                {
+                    return true;
+                }
+
+                // If not existing expire cookie
+                cookie.Expired = true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Return session value by cookie key
+        /// </summary>
+        /// <param name="cookieKey">Cookie key (string)</param>
+        /// <returns>Session value (string)</returns>
+        public string Get(string cookieKey)
+        {
+            if (Exist(cookieKey))
+            {
+                string sessionKey = GetCookie(cookieKey).Value;
+
+                return session[sessionKey];
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Edit session value
+        /// </summary>
+        /// <param name="cookieKey">Cookie key (String)</param>
+        /// <param name="newValue">New value (string)</param>
+        /// <returns>Validation Result (bool)</returns>
+        public bool Edit(string cookieKey, string newValue)
+        {
+            if (Exist(cookieKey))
+            {
+                string sessionKey = GetCookie(cookieKey).Value;
+
+                session[sessionKey] = newValue;
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Get cookie by key from request or response, if not exist it return null
+        /// </summary>
+        /// <param name="cookieKey">Cookie key (string)</param>
+        /// <returns>Cookie object</returns>
+        private Cookie GetCookie(string cookieKey)
+        {
+            if ((request.Cookies[cookieKey] != null) && !request.Cookies[cookieKey].Expired)
+            {
+                return request.Cookies[cookieKey];
+            }
+            else if((response.Cookies[cookieKey] != null) && !response.Cookies[cookieKey].Expired)
+            {
+                return response.Cookies[cookieKey];
+            }
+
+            return null;
+        }
+    }
+}
--- Src/ChibiWebserver/ChibiWebserver/WebServer.cs
+++ Src/ChibiWebserver/ChibiWebserver/WebServer.cs
@@ -11,7 +11,6 @@ using System.Text;
 using System.Threading;
 using System.Web;
 using System.Security.Cryptography;
-using System.Text.RegularExpressions;
 
 namespace ChibiWebserver
 {
@@ -20,8 +19,9 @@ namespace ChibiWebserver
         private HttpListener listener;
         private string rootDirectory;
         private Thread serverThread;
-        private Dictionary<string, int> session;
-        private int lastSessionIndex;
+        private Session session;
+        private DateTime lastUpStart;
+        private int CounterSinceRestart;
 
         // Index files, to show on a directory root
         private readonly string[] indexFiles = {
@@ -40,6 +40,11 @@ namespace ChibiWebserver
             ".ico"
         };
 
+        private readonly string[] specialPages = new string[]
+        {
+            "counter/", "dynamic/", "info/"
+        };
+
         /// <summary>
         /// Constructor with prefixes
         /// </summary>
@@ -55,10 +60,6 @@ namespace ChibiWebserver
             // Set root directory
             rootDirectory = Directory.GetCurrentDirectory() + "/../../../../../Content/";
 
-            // Declare session Dictonary
-            session = new Dictionary<string, int>();
-            int lastSessionIndex = 0;
-
             // URI prefixes are required,
             // for example "http://localhost.com:8080/".
             if (prefixes == null || prefixes.Length == 0)
@@ -72,6 +73,15 @@ namespace ChibiWebserver
             {
                 listener.Prefixes.Add(s);
             }
+
+            // Set server up start time
+            lastUpStart = DateTime.Now;
+
+            // Declare session object
+            session = new Session(lastUpStart.ToString("yyyyMMddHHmmssfff"));
+
+            // Set counter to zero
+            CounterSinceRestart = 0;
         }
 
         /// <summary>
@@ -88,7 +98,17 @@ namespace ChibiWebserver
         }
 
         /// <summary>
-        /// Listen to web ´requests
+        /// Stop webserver
+        /// </summary>
+        public void Stop()
+        {
+            listener.Stop();
+            listener.Close();
+            Console.WriteLine("Webserver is shutdown");
+        }
+
+        /// <summary>
+        /// Listen to web requests
         /// </summary>
         public void Listen()
         {
@@ -112,7 +132,73 @@ namespace ChibiWebserver
         /// <summary>
         /// Process browser request
         /// </summary>
+        /// <param name="context">Web context (HttpListenerContext)</param>
         public void Process(HttpListenerContext context)
+        {
+            // Obtain Request and Response objects.
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+
+            // Get a response stream
+            Stream output = response.OutputStream;
+
+            // Initialize session
+            session.Initialize(context);
+
+            // Web path and filename ex. blog/index.html
+            string webpath = request.Url.AbsolutePath;
+            webpath = webpath.Substring(1);
+
+            CounterSinceRestart++;
+
+            // Check if we got existing counter session, then increase value
+            if (session.Exist("counter"))
+            {
+                int counter;
+
+                if (int.TryParse(session.Get("counter"), out counter))
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter = 1;
+                }
+
+                session.Edit("counter", counter.ToString());
+            }
+            else
+            {
+                session.Add("1");
+            }
+
+            try
+            {
+                if (specialPages.Contains(webpath))
+                {
+                    SpecialPageResponse(context, webpath);
+                }
+                else
+                {
+                    FileResponse(context, webpath);
+                }
+            }
+            catch
+            {
+                WriteLog(webpath, 500);
+                response.StatusCode = (int)HttpStatusCode.InternalServerError; // 500
+
+                // You must close the output stream.
+                output.Close();
+            }
+        }
+
+        /// <summary>
+        /// Response special page
+        /// </summary>
+        /// <param name="context">Web context (HttpListenerContext)</param>
+        /// <param name="webpath">Request web path (string)</param>
+        public void SpecialPageResponse(HttpListenerContext context, string webpath)
         {
             // Obtain request and response objects.
             HttpListenerRequest request = context.Request;
@@ -121,92 +207,80 @@ namespace ChibiWebserver
             // Get a response stream
             Stream output = response.OutputStream;
 
-            // Web path and filename ex. blog/index.html
-            string webpath = context.Request.Url.AbsolutePath;
-            webpath = webpath.Substring(1);
+            string responseText = null;
 
-            string sessionKey;
-
-            if (request.Cookies["counter"] == null)
+            switch (webpath)
             {
-                lastSessionIndex++;
-                sessionKey = lastSessionIndex.ToString();
-                session.Add(sessionKey, 1);
-                response.SetCookie(new Cookie("counter", sessionKey, "/"));
-            }
-            else
-            {
-                sessionKey = request.Cookies["counter"].Value;
-                session[sessionKey]++;
-            }
+                case "counter/":
 
-            if (webpath == "counter/")
-            {
-                Console.WriteLine("Request on " + webpath + " return 200");
+                    string counter = session.Get("counter");
+                    responseText = string.Format("<html><body>{0}</body></html>", counter);
 
-                int counter = session[sessionKey];
-                string responseText = string.Format("<html><body>{0}</body></html>", counter);
+                    response.ContentType = "text/html";
 
-                byte[] buffer = Encoding.UTF8.GetBytes(responseText);
+                    break;
+                case "dynamic/":
 
-                response.ContentType = "text/html";
-                response.ContentLength64 = buffer.Length;
+                    int sum = 0;
 
-                output.Write(buffer, 0, buffer.Length);
-
-            }
-            else
-            {
-                string fileSource = GetRealFileSource(ref webpath);
-
-                Console.Write("Request on " + webpath + " return ");
-
-                // Does file exist?
-                if (File.Exists(fileSource))
-                {
-                    string fileExtension = Path.GetExtension(webpath);
-
-                    // Check if file extension is allowed
-                    if (allowedFileExtensions.Contains(fileExtension))
+                    if (request.QueryString.Count > 0)
                     {
-                        try
+                        int input1;
+                        int input2;
+
+                        if (request.QueryString["input1"] != null)
                         {
-                            DateTime date = DateTime.Now;
-                            DateTime expiresDate = date.Add(new TimeSpan(365, 0, 0, 0)); // Year from now
 
-                            // Read data from file
-                            byte[] fileData = File.ReadAllBytes(fileSource);
-
-                            Console.WriteLine("200");
-
-                            // Construct headers
-                            response.ContentType = MimeMapping.GetMimeMapping(fileSource);
-                            response.ContentLength64 = fileData.Length;
-                            response.AddHeader("Date", date.ToString("r"));
-                            response.AddHeader("Expires", expiresDate.ToString("r"));
-                            response.AddHeader("Last-Modified", File.GetLastWriteTime(fileSource).ToString("r"));
-                            response.AddHeader("Etag", MD5Hash(fileData));
-
-                            // Write the response to output stream.
-                            output.Write(fileData, 0, fileData.Length);
+                            if (int.TryParse(request.QueryString["input1"], out input1))
+                            {
+                                sum = input1;
+                            }
                         }
-                        catch (Exception ex)
+
+                        if (request.QueryString["input2"] != null)
                         {
-                            Console.WriteLine("500");
-                            response.StatusCode = (int)HttpStatusCode.InternalServerError; // 500
+                            if (int.TryParse(request.QueryString["input2"], out input2))
+                            {
+                                sum += input2;
+                            }
                         }
+                    }
+
+                    if (request.Headers.Get("Accept") == "application/xml")
+                    {
+                        responseText = string.Format("<result><value>{0}</value></result>", sum);
+                        response.ContentType = "application/xml";
                     }
                     else
                     {
-                        Console.WriteLine("415");
-                        response.StatusCode = (int)HttpStatusCode.UnsupportedMediaType; // 415
+                        responseText = string.Format("<html><body>{0}</body></html>", sum);
+                        response.ContentType = "text/html";
                     }
-                }
-                else
-                {
-                    Console.WriteLine("404");
-                    response.StatusCode = (int)HttpStatusCode.NotFound; // 404
-                }
+
+                    break;
+
+                case "info/":
+
+                    string lastRestart = lastUpStart.ToString("yyyy-MM-dd hh:mm:ss");
+                    responseText = string.Format("<html><body>Last restart: {0}<br />Up time: {1}<br />Total requests since restart: {2}<br />Sessions count: {3}</body></html>", lastRestart, UpTime(), CounterSinceRestart, session.Count);
+
+                    response.ContentType = "text/html";
+                    break;
+
+                default:
+
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    WriteLog(webpath, 404);
+
+                    break;
+            }
+            
+            if(responseText != null)
+            {
+                byte[] textBuffer = Encoding.UTF8.GetBytes(responseText);
+                response.ContentLength64 = textBuffer.Length;
+                output.Write(textBuffer, 0, textBuffer.Length);
+                WriteLog(webpath, 200);
             }
 
             // You must close the output stream.
@@ -214,13 +288,61 @@ namespace ChibiWebserver
         }
 
         /// <summary>
-        /// Stop webserver
+        /// File request, response with file if it exist
         /// </summary>
-        public void Stop()
+        /// <param name="context">Web context (HttpListenerContext)</param>
+        /// <param name="webpath">Request web path (string)</param>
+        private void FileResponse(HttpListenerContext context, string webpath)
         {
-            listener.Stop();
-            listener.Close();
-            Console.WriteLine("Webserver is shutdown");
+            // Obtain request and response objects.
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+
+            // Get a response stream
+            Stream output = response.OutputStream;
+
+            string fileSource = GetRealFileSource(ref webpath);
+
+            // Does file exist?
+            if (File.Exists(fileSource))
+            {
+                string fileExtension = Path.GetExtension(webpath);
+
+                // Check if file extension is allowed
+                if (allowedFileExtensions.Contains(fileExtension))
+                {
+                    DateTime date = DateTime.Now;
+                    DateTime expiresDate = date.Add(new TimeSpan(365, 0, 0, 0)); // Year from now
+
+                    // Read data from file
+                    byte[] fileData = File.ReadAllBytes(fileSource);
+
+                    // Construct headers
+                    response.ContentType = MimeMapping.GetMimeMapping(fileSource);
+                    response.ContentLength64 = fileData.Length;
+                    response.AddHeader("Date", date.ToString("r"));
+                    response.AddHeader("Expires", expiresDate.ToString("r"));
+                    response.AddHeader("Last-Modified", File.GetLastWriteTime(fileSource).ToString("r"));
+                    response.AddHeader("Etag", MD5Hash(fileData));
+
+                    // Write the response to output stream.
+                    output.Write(fileData, 0, fileData.Length);
+                    WriteLog(webpath, 200);
+                }
+                else
+                {
+                    WriteLog(webpath, 415);
+                    response.StatusCode = (int)HttpStatusCode.UnsupportedMediaType;
+                }
+            }
+            else
+            {
+                WriteLog(webpath, 404);
+                response.StatusCode = (int)HttpStatusCode.NotFound;
+            }
+
+            // You must close the output stream.
+            output.Close();
         }
 
         /// <summary>
@@ -237,7 +359,6 @@ namespace ChibiWebserver
             {
                 foreach (string indexFile in indexFiles)
                 {
-                    //Console.WriteLine(Path.Combine(fileSource, indexFile));
                     if (File.Exists(Path.Combine(fileSource, indexFile)))
                     {
                         webpath = webpath + indexFile;
@@ -248,6 +369,16 @@ namespace ChibiWebserver
             }
 
             return fileSource;
+        }
+
+        /// <summary>
+        /// Write request with status code the serverlog
+        /// </summary>
+        /// <param name="webpath">Requested Web path (string)</param>
+        /// <param name="statusCode">Returning status code (int)</param>
+        public void WriteLog(string webpath, int statusCode)
+        {
+            Console.WriteLine(string.Format("Request on {0} return {1}", webpath, statusCode));
         }
 
         /// <summary>
@@ -278,23 +409,18 @@ namespace ChibiWebserver
         }
 
         /// <summary>
-        /// Make a unique session key
+        /// Return server up time
         /// </summary>
-        /// <returns></returns>
-        private string NewUniqueSessionKey()
+        /// <returns>Server up time (string)</returns>
+        private string UpTime()
         {
-            string sessionKey;
-            Guid keyGuid;
+            TimeSpan dateDiffrence = DateTime.Now - lastUpStart;
+            int days = (dateDiffrence).Days;
+            int hours = (dateDiffrence).Hours;
+            int minutes = (dateDiffrence).Minutes;
+            int seconds = (dateDiffrence).Seconds;
 
-            do
-            {
-                keyGuid = Guid.NewGuid();
-                sessionKey = Convert.ToBase64String(keyGuid.ToByteArray());
-                sessionKey = Regex.Replace(sessionKey, @"[^0-9a-zA-Z]+", ""); // Remove any specialcharacters
-                sessionKey = sessionKey.Substring(0, 12); // Only twelv chars
-            } while (session.ContainsKey(sessionKey));
-
-            return sessionKey;
+            return string.Format("Days {0} | Hours {1} | Minutes {2} | Seconds {3}", days, hours, minutes, seconds);
         }
     }
 }
